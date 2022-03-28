0bbc1e7f787d8f30507f5ad4eaa4f2bdf31304db
Alexander <alez.grante@gmail.com>
2022-03-28 21:07:21 +0300

    SetUp Convenient names

---
+++ app/src/main/java/com/example/funday/ui/home/JokesFragment.kt
@@ -0,0 +1,21 @@
+package com.example.funday.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.funday.databinding.FragmentJokesBinding
+
+class JokesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding = FragmentJokesBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
