521bca53148fd7192e7f21e79154556a917b04c9
Alexander <alez.grante@gmail.com>
2022-03-29 00:36:29 +0300

    BaseViewModel with di

--- app/src/main/java/com/example/funday/presentation/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/funday/presentation/menu/MenuViewModel.kt
@@ -1,13 +1,8 @@
 package com.example.funday.presentation.menu
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class MenuViewModel : ViewModel() {
+class MenuViewModel @Inject constructor() : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
-    }
-    val text: LiveData<String> = _text
 }
